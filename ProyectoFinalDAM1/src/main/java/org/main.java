9732e659359d0b24f98279ae723d8009396fb6ab
Misterioso69 <ikercruzmar2005@gmail.com>
2025-05-18 22:27:39 +0200

    Update

--- ProyectoFinalDAM1/src/main/java/org/main.java
+++ ProyectoFinalDAM1/src/main/java/org/main.java
@@ -19,9 +19,9 @@ public class main {
 
                 "Importante en la base de datos tiene " +
                         "que existir el usuario: prueba y" +
-                        "la contraseña tiene que ser 1234. Tambien se " +
+                        "la contraseña tiene que ser 1234.\n Tambien se " +
                         "tiene que haber ejecutado el archivo sql" +
-                        "agregado junto al proyecto /SQL/BaseDatos.sql",
+                        "agregado junto al proyecto /SQL/BaseDatos.sql .",
                 "Leer antes de Jugar",
                 JOptionPane.WARNING_MESSAGE
         );
